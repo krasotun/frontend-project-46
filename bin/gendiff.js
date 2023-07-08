f993ed86a2d894375dd5bec9fa5f47bb4be3de2e
krasotun <servrostov@gmail.com>
2023-07-08 19:35:11 +0300

    add first step

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("gendiff")
+  .description("Compares two configuration files and shows a difference.")
+  .option("-v, --version", "output the version number")
+  .option("-f, --format <type>", "output format", "stylish");
+
+program.parse();
