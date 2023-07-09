ef6bd82e5c0cf44f0d3b9a17efde0026320bd2cd
krasotun <servrostov@gmail.com>
2023-07-09 14:52:32 +0300

    add comparator

--- src/comparer.js
+++ src/comparer.js
@@ -1,3 +1,40 @@
 export default (object1, object2) => {
-  return { object1, object2 };
+  const result = [];
+
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+
+  for (const key of object1Keys) {
+    if (object2.hasOwnProperty(key)) {
+      if (object1[key] === object2[key]) {
+        result.push(generateString(" ", key, object1[key]));
+      } else {
+        result.push(generateString("-", key, object1[key]));
+        result.push(generateString("+", key, object2[key]));
+      }
+    } else {
+      result.push(generateString("-", key, object1[key]));
+    }
+  }
+
+  for (const key of object2Keys) {
+    if (!object1.hasOwnProperty(key)) {
+      result.push(generateString("+", key, object2[key]));
+    }
+  }
+  return result
+    .sort(([, keyA], [, keyB]) => {
+      if (keyA < keyB) {
+        return -1;
+      }
+      if (keyA > keyB) {
+        return 1;
+      }
+
+      return 0;
+    })
+    .map((element) => element.join(" "))
+    .join("\n");
 };
+
+const generateString = (polar, key, value) => [polar, `${key}: ${value}`];
