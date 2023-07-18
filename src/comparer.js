89a87f3943f1f4e82d6202575daa40d5f2de9b87
krasotun <servrostov@gmail.com>
2023-07-18 10:16:46 +0300

    ref comparer

--- src/comparer.js
+++ src/comparer.js
@@ -1,28 +1,52 @@
-const generateString = (polar, key, value) => [polar, `${key}: ${value}`];
-
-export default (object1, object2) => {
-  const result = [];
-
+const getSortedKeys = (object1, object2) => {
   const object1Keys = Object.keys(object1);
   const object2Keys = Object.keys(object2);
-
-  object1Keys.forEach((key) => {
-    if (Object.hasOwn(object2, key)) {
-      if (object1[key] === object2[key]) {
-        result.push(generateString(' ', key, object1[key]));
-      } else {
-        result.push(generateString('-', key, object1[key]));
-        result.push(generateString('+', key, object2[key]));
-      }
-    } else {
-      result.push(generateString('-', key, object1[key]));
-    }
-  });
-
-  object2Keys.forEach((key) => {
-    if (!Object.hasOwn(object1, key)) {
-      result.push(generateString('+', key, object2[key]));
-    }
-  });
-  return result.map((element) => element.join(' ')).join('\n');
+  const unionKeys = [...object1Keys, ...object2Keys];
+  return unionKeys.sort();
 };
+
+const comparer = (object1, object2) => {
+  const keys = getSortedKeys(object1, object2);
+
+  const result = keys.map((key) => {
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    if (Object.hasOwn(object1, key) && !Object.hasOwn(object2, key)) {
+      return {
+        key,
+        value1,
+        status: 'deleted',
+      };
+    }
+
+    if (!Object.hasOwn(object1, key) && Object.hasOwn(object2, key)) {
+      return {
+        key,
+        value2,
+        status: 'added',
+      };
+    }
+
+    if (value1 === value2) {
+      return {
+        key,
+        value1,
+        status: 'unchanged',
+      };
+    }
+
+    return {
+      key,
+      value1,
+      value2,
+      status: 'changed',
+    };
+  });
+
+  return result;
+};
+
+//   return result.map((element) => element.join(' ')).join('\n');
+
+export default comparer;
